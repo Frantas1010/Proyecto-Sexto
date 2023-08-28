25d1fe371645726b5bd5475e35c574dcc23cf540
gastoon16 <92887798+Frantas1010@users.noreply.github.com>
2023-08-28 15:58:28 -0300

    full delete

--- controllers/direcciones.js
+++
@@ -1,8 +0,0 @@
-exports.inicio = (req, res) => {
-    mostrarRecuadro = false;
-    res.render("home", { tit: "", cont: "", mostrarRecuadro });
-};
-
-exports.form = (req, res) => {
-    res.render("formulario");
-};
